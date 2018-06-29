57685d0a0987874143308e3a75fdadd262055344
Jun Hue <junhue.lim@qanvast.com>
2018-06-29 18:36:00 +0800

    Setup App and test environment

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { ApplicationModule } from './app.module';
+import { middleware } from './common/middlewares/logger.middleware';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { logger } from './common/logger/logger';
+import { configDevelop } from './config/development';
+import { configProduction } from './config/production';
+
+let config;
+switch (process.env.NODE_ENV) {
+case 'production':
+    config = configProduction;
+    break;
+default:
+    config = configDevelop;
+}
+
+const bootstrap = async () => {
+    const app = await NestFactory.create(ApplicationModule);
+    app.use(middleware);
+    app.useGlobalFilters(new HttpExceptionFilter());
+    await app.listen(8300, () => logger.info(`Server started on http://${config.host}:${config.port}`));
+};
+
+bootstrap();

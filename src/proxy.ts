560d44510b1b187a14651e0a01196b778948a7c9
Jun Hue <junhue.lim@qanvast.com>
2018-07-01 14:37:45 +0800

    Updated proxy to include /stock

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,6 @@
 export const proxyDomain = 'http://localhost:';
 
 export const proxyList = [
-    { uri: '/cats', port: 8301 }
+    { uri: '/cats', port: 8301 },
+    { uri: '/stock', port: 8302 }
 ];

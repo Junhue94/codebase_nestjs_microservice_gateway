2ef35a68875c9c36cdac7b7c35fd4891d2acc128
Junhue94 <13scjunhue@gmail.com>
2018-07-02 23:21:47 +0800

    Update add-stock-table migration script for enums

--- migrations/scripts/20180702144423-add-stock-table.js
+++ migrations/scripts/20180702144423-add-stock-table.js
@@ -4,15 +4,50 @@ module.exports = {
             id: {
                 type: Sequelize.INTEGER,
                 primaryKey: true,
-                autoIncrement: true,
+                autoIncrement: true
             },
-            type: Sequelize.STRING,
+            type: {
+                type: Sequelize.ENUM,
+                values: [
+                    'Current',
+                    'Watchlist',
+                    'Partial Exit',
+                    'Exited'
+                ]
+            },
             entryDate: Sequelize.DATE,
             exitDate: Sequelize.DATE,
             name: Sequelize.STRING,
-            sector: Sequelize.STRING,
-            country: Sequelize.STRING,
-            currency: Sequelize.STRING,
+            sector: Sequelize.ENUM(
+                'Basic Material',
+                'Consumer Staple',
+                'Consumer Discretionary',
+                'Energy',
+                'Entertainment',
+                'Financial',
+                'Health Care',
+                'Industrial',
+                'Property',
+                'Service',
+                'Technology',
+                'Telecommunication',
+                'Transportation',
+                'Utilities',
+                'REIT',
+                'Business Trust'
+            ),
+            country: Sequelize.ENUM(
+                'SG',
+                'MY',
+                'HK',
+                'US'
+            ),
+            currency: Sequelize.ENUM(
+                'S$',
+                'RM',
+                'HKD',
+                'US$'
+            ),
             priceBuy: Sequelize.FLOAT,
             priceSell: Sequelize.FLOAT,
             priceProfitTarget: Sequelize.FLOAT,
@@ -32,3 +67,7 @@ module.exports = {
         return queryInterface.dropTable('Stock');
     }
 };
+
+// TODO Add ENum
+// https://stackoverflow.com/questions/45437924/drop-and-create-enum-with-sequelize-correctly
+// https://github.com/sequelize/sequelize/issues/2554
